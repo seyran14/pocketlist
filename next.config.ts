3940be961addaa70d3443ae6c4dc78616a9c5d5b
seyran14 <saparovseyran@gmail.com>
2026-05-22 16:26:27 +0400

    Add Sentry monitoring

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import { withSentryConfig } from "@sentry/nextjs"
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.142"],
   devIndicators: false,
-};
+}
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: true,
+  telemetry: false,
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+})
